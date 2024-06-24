c9cc1df3dd7b516a55f74afe04f50793de86b69d
EmreSani <emresani1998@gmail.com>
2024-06-24 19:38:56 +0300

    swagger updated

--- src/main/java/com/project/ecommerce/config/OpenAPIConfig.java
+++ src/main/java/com/project/ecommerce/config/OpenAPIConfig.java
@@ -8,7 +8,7 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "StudentManagement API", version = "1.0.0"),
+@OpenAPIDefinition(info = @Info(title = "E Commerce API", version = "1.0.0"),
         security = @SecurityRequirement(name = "Bearer"))
 @SecurityScheme(name = "Bearer", type = SecuritySchemeType.HTTP, scheme = "Bearer")
 public class OpenAPIConfig {
